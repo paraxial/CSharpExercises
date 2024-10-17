2045a58116bad2becb5add7110dd601791a592b9
Paraxial O'Kelly <9346424+paraxial@users.noreply.github.com>
2024-10-17 12:44:44 +0800

    [01] Copy Math minigame

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,2 +1,189 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using MathGame;
+
+MathGameLogic mathGame = new MathGameLogic();
+Random random = new Random();
+
+int firstNumber;
+int secondNumber;
+int userMenuSelection;
+
+int score = 0;
+bool gameOver = false;
+DifficultyLevel difficultyLevel = DifficultyLevel.Easy;
+
+while(!gameOver)
+{
+  userMenuSelection = GetUserMenuSelection(mathGame);
+  firstNumber = random.Next(1,101);
+  secondNumber = random.Next(1, 101);
+
+  switch(userMenuSelection) {
+    case 1:
+      score += await PerformOperation(mathGame, firstNumber, secondNumber, '+', score, difficultyLevel);
+      break;
+    case 2:
+      score += await PerformOperation(mathGame, firstNumber, secondNumber, '-', score, difficultyLevel);
+      break;
+    case 3:
+      score += await PerformOperation(mathGame, firstNumber, secondNumber, '*', score, difficultyLevel);
+      break;
+    case 4:
+      while( firstNumber % secondNumber != 0) {
+        firstNumber = random.Next(1, 101);
+        secondNumber = random.Next(1, 101);
+      }
+      score += await PerformOperation(mathGame, firstNumber, secondNumber, '/', score, difficultyLevel);
+      break;
+    case 5:
+      int numberOfQuestions = 99;
+      Console.WriteLine("Please enter the number of questions you want to attempt");
+      while(!int.TryParse(Console.ReadLine(), out numberOfQuestions)) {
+        Console.WriteLine("Please enter the number of questions you want to attempt as an integer.");
+      }
+      while(numberOfQuestions > 0) {
+        int randomOperation = random.Next(1,5);
+        firstNumber = random.Next(1, 101);
+        secondNumber = random.Next(1, 101);
+
+        switch (randomOperation)
+        {
+          case 1:
+            score += await PerformOperation(mathGame, firstNumber, secondNumber, '+', score, difficultyLevel);
+            break;
+          case 2:
+            score += await PerformOperation(mathGame, firstNumber, secondNumber, '-', score, difficultyLevel);
+            break;
+          case 3:
+            score += await PerformOperation(mathGame, firstNumber, secondNumber, '*', score, difficultyLevel);
+            break;
+          case 4:
+            while (firstNumber % secondNumber != 0)
+            {
+              firstNumber = random.Next(1, 101);
+              secondNumber = random.Next(1, 101);
+            }
+            score += await PerformOperation(mathGame, firstNumber, secondNumber, '/', score, difficultyLevel);
+            if (randomOperation == 1)
+            {
+              score += await PerformOperation(mathGame, firstNumber, secondNumber, '+', score, difficultyLevel);
+            }
+            break;
+        }
+        numberOfQuestions--;
+      }
+      break;
+    case 6:
+      Console.WriteLine("GAME HISTORY: \n");
+      foreach(string record in mathGame.GameHistory) {
+        Console.WriteLine(record);
+      }
+      break;
+    case 7:
+      difficultyLevel = SetDifficulty();
+      DifficultyLevel difficultyEnum = (DifficultyLevel)difficultyLevel;
+      Enum.IsDefined(typeof(DifficultyLevel), difficultyEnum);
+      Console.WriteLine($"Your new difficulty level is {difficultyLevel}");
+      break;
+    case 8:
+      gameOver = true;
+      Console.WriteLine($"Your final score is {score}");
+      break;
+  }
+}
+
+
+static DifficultyLevel SetDifficulty()
+{
+  int userSelection = 0;
+  Console.WriteLine("Please enter a difficulty level:");
+  Console.WriteLine("1. Easy");
+  Console.WriteLine("2. Medium");
+  Console.WriteLine("3. Hard");
+
+  while(!int.TryParse(Console.ReadLine(), out userSelection) && userSelection < 1 || userSelection > 3) {
+    Console.WriteLine("Please enter a valid option 1-3");
+  }
+
+  return (userSelection) switch {
+      1 => DifficultyLevel.Easy,
+      2 => DifficultyLevel.Medium,
+      3 => DifficultyLevel.Hard,
+      _ => DifficultyLevel.Easy
+  };
+}
+
+static void DisplayMathGameQuestion(int firstNumber, int secondNumber, char operation) {
+  Console.WriteLine($"{firstNumber} {operation} {secondNumber} = ?");
+}
+
+static int GetUserMenuSelection(MathGameLogic mathGame) {
+  int selection = -1;
+  mathGame.ShowMenu();
+  while(selection < 1 || selection > 8) {
+    while(!int.TryParse(Console.ReadLine(), out selection)) {
+      Console.WriteLine("Please enter a valid option 1-8");
+    }
+
+    if(!(selection >=1 && selection <= 8)) {
+      Console.WriteLine("Please enter a valid option 1-8");
+    }
+  }
+
+  return selection;
+}
+
+static async Task<int?> GetUserResponse(DifficultyLevel difficulty)
+{
+  int response = 0;
+  int timeout = (int) difficulty;
+
+  Stopwatch stopwatch = new Stopwatch();
+  stopwatch.Start();
+  Task<string?> getUserInputTask = Task.Run(() => Console.ReadLine());
+
+  try {
+    string? result = await Task.WhenAny(getUserInputTask, Task.Delay(timeout * 1000)) == getUserInputTask ? getUserInputTask.Result : null;
+    stopwatch.Stop();
+    if(result != null && int.TryParse(result, out response)) {
+      string elapsedTime = stopwatch.Elapsed.ToString("m\\:ss\\.ff");
+      Console.WriteLine($"Time taken to answer {elapsedTime}");
+      return response;
+    } else {
+      throw new OperationCanceledException();
+    }
+  } catch(OperationCanceledException) {
+    Console.WriteLine("Time is up!");
+  }
+
+  return null;
+}
+
+static int ValidateResponse(int result, int? userResponse, int score) {
+  if(result == userResponse) {
+    Console.WriteLine("You answered correctly! You earn 5 points.");
+    score += 5;
+  } else {
+    Console.WriteLine($"Incorrect.  The correct answer was {result}");
+  }
+
+  return score;
+}
+
+static async Task<int> PerformOperation(MathGameLogic mathGame, int firstNumber, int secondNumber, char operation, int score, DifficultyLevel difficulty)
+{
+  int result;
+  int? userResponse;
+  DisplayMathGameQuestion(firstNumber, secondNumber, operation);
+  result = mathGame.MathOperation(firstNumber, secondNumber, operation);
+  userResponse = await GetUserResponse(difficulty);
+  score += ValidateResponse(result, userResponse, score);
+
+  return score;
+}
+
+public enum DifficultyLevel {
+  Easy = 45,
+  Medium = 30,
+  Hard = 15
+};
---
+++ MathGame/README.md
@@ -0,0 +1,15 @@
+# README
+This is 1 of a series of exercises from thecsharpacademy / WillCodeBits.
+There has been no iteration on the exercise as presented -- this was more an excuse to become familiar with C# syntax and flow.
+I may refactor this as an exercise in the next commit.
+
+## Requirements:
+1. You need to create a Math game containing the 4 basic operations
+
+2. The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100. Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer.
+
+3. Users should be presented with a menu to choose an operation
+
+4. You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.
+(You don't need to record results on a database. Once the program is closed the results will be deleted.)
+
